80cb646e128aed0bde6fc4b49f979a467bf22624
theMinorDeveloper <theminordeveloper@outlook.com>
2021-06-06 17:30:18 +0100

    IT WORKS

--- src/main/java/net/entscrew/dolos/Dolos.java
+++ src/main/java/net/entscrew/dolos/Dolos.java
@@ -1,7 +1,35 @@
 package net.entscrew.dolos;
 
-public class Dolos {
-    public static void main(String[] args) {
-        
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Dolos extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Dolos.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
