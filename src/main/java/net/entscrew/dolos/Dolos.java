f7606f8664a6ee6cac4aa76565863a2d14823267
theMinorDeveloper <theminordeveloper@outlook.com>
2021-06-06 12:34:38 +0100

    Added dependencies

--- src/main/java/net/entscrew/dolos/Dolos.java
+++ src/main/java/net/entscrew/dolos/Dolos.java
@@ -2,5 +2,6 @@ package net.entscrew.dolos;
 
 public class Dolos {
     public static void main(String[] args) {
+        
     }
 }
---
+++ src/test/java/net/entscrew/dolos/DolosTest.java
@@ -0,0 +1,12 @@
+package net.entscrew.dolos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DolosTest {
+
+    @Test
+    void main() {
+    }
+}
